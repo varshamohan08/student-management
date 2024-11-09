8cad8d6fe4e697d94683332dbdd30099788961a7
Varsha Mohan <37172702+varshamohan08@users.noreply.github.com>
2024-11-09 23:04:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     DB_NAME = "student_management"
-    DB_USER = "postgres"
-    DB_PASSWORD = "v@123"
+    DB_USER = "your_db_user"
+    DB_PASSWORD = "your_db_password"
     DB_HOST = "localhost"
     DB_PORT = "5432"
